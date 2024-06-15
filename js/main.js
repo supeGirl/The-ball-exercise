a38dd61e9957f0d25b1e8eae739c614e55a16664
supaGirl <kerenvasserman.bm@gmail.com>
2024-06-16 00:25:10 +0300

    Add a maxDiameter parameter to onBallClick() and change the first ball color

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 'use strict'
 
-function onBallClick() {
+function onBallClick(maxDiameter, ballClass) {
 
-  var elBall1 = document.querySelector('.ball')
-  var currMeasure = elBall1.offsetWidth // using vanilla js to get current width
+  var elBall = document.querySelector('.' + ballClass)
+  var currMeasure = elBall.offsetWidth // using vanilla js to get current width
   var randomInt = getRandomInt(20, 61)
 
   var newMeasure = currMeasure + randomInt
 
-  if (newMeasure > 400) newMeasure = 100
+  if (newMeasure > maxDiameter) newMeasure = 100
 
-  elBall1.style.backgroundColor = getRandomColor()
+  elBall.style.backgroundColor = getRandomColor()
   
-  elBall1.style.width = newMeasure + 'px'
-  elBall1.style.height = newMeasure + 'px'
+  elBall.style.width = newMeasure + 'px'
+  elBall.style.height = newMeasure + 'px'
   
-  elBall1.innerText = newMeasure
+  elBall.innerText = newMeasure
 }
