6b0d0db8524b153a9838f8758bbf6b5626dced26
supaGirl <kerenvasserman.bm@gmail.com>
2024-06-16 00:04:31 +0300

    Fix lines

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 'use strict'
 
-function onBallClick(){
+function onBallClick() {
+
   var elBall1 = document.querySelector('.ball')
   var currMeasure = elBall1.offsetWidth // using vanilla js to get current width
-var randomInt = getRandomInt(20,61)
+  var randomInt = getRandomInt(20, 61)
 
   var newMeasure = currMeasure + randomInt
 
-  if(newMeasure > 400) newMeasure = 100
+  if (newMeasure > 400) newMeasure = 100
 
+  elBall1.style.backgroundColor = getRandomColor()
+  
   elBall1.style.width = newMeasure + 'px'
   elBall1.style.height = newMeasure + 'px'
-  elBall1.style.backgroundColor = getRandomColor()
-  elBall1.innerText = newMeasure 
+  
+  elBall1.innerText = newMeasure
 }
